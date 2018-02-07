14d26cfdc23cc513d9cad470a28728423fd38c44
Justin Laureano <gravatron87@gmail.com>
2018-02-07 12:07:26 -0500

    First commit to News Feed site

---
+++ mynewsfeed/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class RedditPost(models.Model):
+    title = models.CharField(max_length=500)
+    url = models.CharField(max_length=500, blank=True)
+    comments = models.CharField(max_length=500, blank=True)
+    subreddit = models.CharField(max_length=500, blank=True)
+
+
+    def __str__(self):
+        return self.title
+
